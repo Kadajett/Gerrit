aabb9640cdaf29d707ba02f43d277c6135e85e52
kadajett <jeremy.ryan.stover@gmail.com>
2013-10-23 16:04:16 -0400

    adds index file and dependencies.

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gerrit - Source Testing for the Future</title>
+	<link rel="stylesheet" type="text/css" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<script type="text/javascript" src="../bower_components/less/dist/less-1.5.0.min.js"></script>
+	<link rel="stylesheet/less" type="text/css" href="css/style.less">
+	<link rel="stylesheet/less" type="text/css" href="css/bootstrapstyles.min.css">
+</head>
+<body>
+
+
+</body>
+<script type="text/javascript" src="../bower_components/jquery/jquery.min.js"></script>
+</html>
